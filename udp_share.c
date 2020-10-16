692b7ba6170f3b745617a22433ed4022ebba11bc
lihf <lihf@genvict.com>
2020-10-16 17:27:38 +0800

    提交udp广播的测试代码和测试结果

--- udp_share.c
+++ udp_share.c
@@ -1,4 +1,19 @@
 //UDP单播，能发能收
+/*
+    实现广播，离不开广播地址，同一个子网（局域网）的所有主机网卡都会接收所在网段广播地址的数据包。广播地址应用于局域网内的
+所有主机。广播地址(Broadcast Address)是专门用于同时向网络中（通常指同一子网）所有工作站进行发送的一个地址。
+
+    广播UDP与单播UDP的区别就是IP地址不同，广播使用广播地址255.255.255.255，将消息发送到在同一广播网络上的每个主机。值得强调
+的是：本地广播信息是不会被路由器转发。当然这是十分容易理解的，因为如果路由器转发了广播信息，那么势必会引起网络瘫痪。
+
+    其实广播顾名思义，就是想局域网内所有的人说话，但是广播还是要指明接收者的端口号的，因为不可能接受者的所有端口都来收听广播。
+
+【UDP广播特点如下】：
+
+1)可以向广播域内的所有主机发送数据；
+2)不能够跨越不同的网络，被路由器所隔离开。
+
+*/
 
 #include <stdio.h>
 #include <sys/socket.h>
@@ -6,23 +21,13 @@
 #include <string.h>
 #include <pthread.h>
 
+#define BROADCAST_ID ("192.168.6.255")
+#define UDP_PORT (1188)
+
 void *recv_handle(void *arg);
 
 int main(int argc, int *argv[])
 {
-    if(argc != 2)
-    {
-        printf("Please input the port!\n");
-        return -1;
-    }
-
-    int port = atoi(argv[1]);
-    if((port<1025) || (port>65535))
-    {
-        printf("The port is illegal! Please let it in[1025, 65535]\n");
-        return -1;
-    }
-
     int udp_socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(udp_socket_fd < 0)
     {
@@ -30,12 +35,21 @@ int main(int argc, int *argv[])
         return -1;
     }
 
+    int enable = 1;
+    int ret = setsockopt(udp_socket_fd, SOL_SOCKET, SO_BROADCAST, &enable, sizeof(enable));
+    if(ret < 0)
+    {
+        printf("Set setsockopt error!\n");
+        close(udp_socket_fd);
+        return -1;
+    }
+
     struct sockaddr_in local_addr = {0};
     local_addr.sin_family = AF_INET;         //使用IPV4协议
-    local_addr.sin_port = htons(port);
+    local_addr.sin_port = htons(UDP_PORT);
     local_addr.sin_addr.s_addr = INADDR_ANY; //让系统自动检测本地网卡，自动绑定本地ip
 
-    int ret = bind(udp_socket_fd, (struct sockaddr*)&local_addr, sizeof(local_addr));
+    ret = bind(udp_socket_fd, (struct sockaddr*)&local_addr, sizeof(local_addr));
     if(ret < 0)
     {
         printf("Bind socket failed!\n");
@@ -47,7 +61,6 @@ int main(int argc, int *argv[])
     pthread_create(&recv_phread, NULL, recv_handle, (void *)&udp_socket_fd);
 
     struct sockaddr_in dst_addr = {0};
-    char dest_ip[32]={0};
     int dest_port = 0;  //之前改成short类型，会导致发不出
     char msg[1024] = {0};
     int always_true = 1;
@@ -56,22 +69,14 @@ int main(int argc, int *argv[])
 
     while(always_true)
     {
-        printf("Send format:ip port msg\n");
-        memset(dest_ip, 0, sizeof(dest_port));
+        printf("Send msg bellow:\n");
         memset(msg, 0, sizeof(msg));
-        scanf("%s %d %s", dest_ip, &dest_port, msg);  //输入的msg不要用空格，否则会放到下一次的输入
-
-        if((dest_port<1025) || (dest_port>65535))
-        {
-            printf("dest port error!\n");
-            close(udp_socket_fd);
-            pthread_cancel(recv_phread);
-            return -1;
-        }
+        scanf("%s", msg);  //输入的msg不要用空格，否则会放到下一次的输入
 
         dst_addr.sin_family = AF_INET;
-        dst_addr.sin_port = htons(dest_port);
-        dst_addr.sin_addr.s_addr = inet_addr(dest_ip);
+        dst_addr.sin_port = htons(UDP_PORT);
+        //dst_addr.sin_addr.s_addr = inet_addr(BROADCAST_ID);  //好像用这个广播地址也可以，最后一个字节255，但是192.168.255.255这个地址不可以
+        dst_addr.sin_addr.s_addr = 0xFFFFFFFFUL;
 
         sendto(udp_socket_fd, msg, strlen(msg), 0, (struct sockaddr*)&dst_addr, sizeof(dst_addr));
 
